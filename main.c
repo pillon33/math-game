6a639d706a734882ab4aa61afb67f304d4e559f2
pillon33 <szymon.pilszek@gmail.com>
2023-01-10 19:36:37 +0100

    Empty window
    Program creates empty window.

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <gtk/gtk.h>
-void tworz_okno(){
-GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-gtk_window_set_default_size(GTK_WINDOW(win),800,600);
-gtk_window_set_title(GTK_WINDOW(win),"Hello world");
-g_signal_connect(G_OBJECT(win), "destroy", gtk_main_quit, NULL);
-gtk_widget_show(win);
+
+//function intializing window
+void init_window()
+{
+    GtkWidget *window;
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "icon");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    gtk_widget_show(window);
+
+    g_signal_connect(G_OBJECT(window), "destroy",
+            G_CALLBACK(gtk_main_quit), NULL);
 }
-int main(int argc, char **argv){
-gtk_init(&argc,&argv);// inicjujemy gtk
-tworz_okno();
-gtk_main();// uruchamia p˛etl˛e główn ˛a programu, która trwa
-// do momentu wywołania w procesie funkcji gtk_main_quit
+
+int main(int argc, char *argv[]) 
+{
+    gtk_init(&argc, &argv);
+
+    init_window();
+
+    gtk_main();
+
+    return 0;
 }
