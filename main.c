27ee4da43cec7bf9e2321357beb04e00cf0af26e
pillon33 <szymon.pilszek@gmail.com>
2023-01-12 20:23:51 +0100

    Progress
    - Added new tabs: Play panel and settings,
    - Separated window initialization from menu initialization

--- main.c
+++ main.c
@@ -3,107 +3,79 @@
 #include <stdlib.h>
 #include <time.h>
 
-int number = 0;
+GtkWidget *window;
+GtkWidget *fixed;
 
-//function updating label with new number value
-void update_label(GtkWidget *label)
+void init_menu();
+
+void back()
 {
-    char *text;
-    text = g_strdup_printf ("Value : %d",number);
-
-    gtk_label_set_label(GTK_LABEL(label), text);
-
-    g_free(text);
+    gtk_widget_destroy(fixed);
+    init_menu();
 }
 
-//increases global number
-void b1(GtkWidget *button, GtkWidget *label)
+// function called when learning game type is chosen
+void nauka()
 {
-    number++;
-
-    update_label(label);
+    // code here //
 }
 
-//decreases global number
-void b2(GtkWidget *button, GtkWidget *label)
+// function called when test game type is chosen
+void test()
 {
-    number--;
-
-    update_label(label);
+    // code here //
 }
 
-//resets global number
-void b3(GtkWidget *button, GtkWidget *label)
+// function called when arcade game type is chosen
+void arcade()
 {
-    number = 0;
-
-    update_label(label);
+    // code here //
 }
 
-//sets global number to random in range (1, 100)
-void b4(GtkWidget *button, GtkWidget *label)
+void init_play()
 {
-    number = rand()%100 + 1;
-
-    update_label(label);
-}
-
-//function intializing menu window
-void init_menu()
-{
-    GtkWidget *window;
-    GtkWidget *fixed;
     GtkWidget *button;
-    GtkWidget *label;
 
-    //setup window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Math Game");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-
-    //initialize grid
+    //initialize fixed
     fixed = gtk_fixed_new();
 
     gtk_container_add(GTK_CONTAINER(window), fixed);
 
-    //create label
-    char *text;
-    text = g_strdup_printf ("Value : %d",number);
-    label = gtk_label_new(text);
-    g_free(text);
-
-    gtk_fixed_put(GTK_FIXED(fixed), label, 340, 100);
-
     //create first button
-    button = gtk_button_new_with_label("Button 1");
-    g_signal_connect (button, "clicked", G_CALLBACK (b1), label);
+    button = gtk_button_new_with_label("Nauka");
+    g_signal_connect (button, "clicked", G_CALLBACK (nauka), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
 
     //attach first button
-    gtk_fixed_put(GTK_FIXED(fixed), button, 340, 150);
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 150);
 
     //create second button
-    button = gtk_button_new_with_label("Button 2");
-    g_signal_connect (button, "clicked", G_CALLBACK (b2), label);
+    button = gtk_button_new_with_label("Test");
+    g_signal_connect (button, "clicked", G_CALLBACK (test), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
 
     //attach second button
-    gtk_fixed_put(GTK_FIXED(fixed), button, 340, 200);
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 200);
 
     //create third button
-    button = gtk_button_new_with_label("Button 3");
-    g_signal_connect (button, "clicked", G_CALLBACK (b3), label);
+    button = gtk_button_new_with_label("Arcade");
+    g_signal_connect (button, "clicked", G_CALLBACK (arcade), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
 
     //attach third button 
-    gtk_fixed_put(GTK_FIXED(fixed), button, 340, 250);
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 250);
 
     //create fourth button
-    button = gtk_button_new_with_label("Button 4");
-    g_signal_connect (button, "clicked", G_CALLBACK (b4), label);
+    button = gtk_button_new_with_label("Wyjscie");
+    g_signal_connect (button, "clicked", G_CALLBACK (back), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
 
     //attach fourth button 
-    gtk_fixed_put(GTK_FIXED(fixed), button, 340, 300);
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 300);
 
     gtk_widget_show_all(window);
 
@@ -111,11 +83,146 @@ void init_menu()
             G_CALLBACK(gtk_main_quit), NULL);
 }
 
+void init_settings()
+{
+    GtkWidget *slider;
+    GtkWidget *label;
+
+    //initialize fixed
+    fixed = gtk_fixed_new();
+
+    gtk_container_add(GTK_CONTAINER(window), fixed);
+
+    label = gtk_label_new("Poziom trudnosci");
+    gtk_fixed_put(GTK_FIXED(fixed), label, 320, 130);
+
+    //initialize first slider
+    slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+
+    //set size
+    gtk_widget_set_size_request(slider, 160, 32);
+
+    gtk_fixed_put(GTK_FIXED(fixed), slider, 310, 150);
+
+    label = gtk_label_new("Dlugosc gry");
+    gtk_fixed_put(GTK_FIXED(fixed), label, 320, 230);
+
+    //initialize second slider
+    slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+
+    //set size
+    gtk_widget_set_size_request(slider, 160, 32);
+
+    gtk_fixed_put(GTK_FIXED(fixed), slider, 310, 250);
+
+    gtk_widget_show_all(window);
+}
+
+//menu button 1 (Play)
+void menu_b1(GtkWidget *button)
+{
+    gtk_widget_destroy(fixed);
+    init_play();
+}
+
+//menu button 2 (Leaderboard)
+void menu_b2(GtkWidget *button, GtkWidget *fixed)
+{
+    // code //
+}
+
+//menu button 3 (Settings)
+void menu_b3(GtkWidget *button, GtkApplication *app)
+{
+    gtk_widget_destroy(fixed);
+    init_settings();
+}
+
+//menu button 4 ()
+void menu_b4(GtkWidget *button, GtkApplication *app)
+{
+    // code here //
+}
+
+void init_window()
+{
+    //setup window
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Math Game");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+}
+
+//function intializing menu window
+void init_menu()
+{
+    GtkWidget *button;
+
+    //initialize fixed
+    fixed = gtk_fixed_new();
+
+    gtk_container_add(GTK_CONTAINER(window), fixed);
+
+    //create first button
+    button = gtk_button_new_with_label("Graj");
+    g_signal_connect (button, "clicked", G_CALLBACK (menu_b1), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
+
+    //attach first button
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 150);
+
+    //create second button
+    button = gtk_button_new_with_label("Wyniki");
+    g_signal_connect (button, "clicked", G_CALLBACK (menu_b2), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
+
+    //attach second button
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 200);
+
+    //create third button
+    button = gtk_button_new_with_label("Ustawienia");
+    g_signal_connect (button, "clicked", G_CALLBACK (menu_b3), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
+
+    //attach third button 
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 250);
+
+    //create fourth button
+    button = gtk_button_new_with_label("Wyjscie");
+    g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
+
+    //attach fourth button 
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 300);
+
+    gtk_widget_show_all(window);
+
+    g_signal_connect(G_OBJECT(window), "destroy",
+            G_CALLBACK(gtk_main_quit), NULL);
+}
+
+
 int main(int argc, char *argv[]) 
 {
     srand(time(NULL));
 
     gtk_init(&argc, &argv);
+
+    // GtkApplication *app;
+    // int status;
+
+    // app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    // g_signal_connect (app, "activate", G_CALLBACK (init_menu), NULL);
+    // status = g_application_run (G_APPLICATION (app), argc, argv);
+    // g_object_unref (app);
+
+    init_window();
 
     init_menu();
 
