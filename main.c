caf29abf66de6736a3d7941e40358b6aebd419b5
pillon33 <szymon.pilszek@gmail.com>
2023-01-14 19:19:40 +0100

    Progress
    changes:
    - changed sliders to global variables
    
    new features:
    - settings are now saved in settings.json file,
    - settings are loaded from settings.json file or are assigned with default values if necessary

--- main.c
+++ main.c
@@ -2,17 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <json-c/json.h>
 
 GtkWidget *window;
 GtkWidget *fixed;
+GtkWidget *slider1;     //Game difficulty slider
+GtkWidget *slider2;     //Game length slider
+
+double difficulty = 1;  //value of game difficulty
+double length = 1;      //value of game length
+
+json_object *ldboard;
 
 void init_menu();
-
-void back()
-{
-    gtk_widget_destroy(fixed);
-    init_menu();
-}
+void init_play();
+void init_settings();
+void settings_back();
+void save_settings();
+void read_settings();
+void init_leaderboard();
+void save_leaderboard();
+void read_leaderboard();
 
 // function called when learning game type is chosen
 void nauka()
@@ -34,6 +44,8 @@ void arcade()
 
 void init_play()
 {
+    gtk_widget_destroy(fixed);
+
     GtkWidget *button;
 
     //initialize fixed
@@ -69,8 +81,8 @@ void init_play()
     gtk_fixed_put(GTK_FIXED(fixed), button, 310, 250);
 
     //create fourth button
-    button = gtk_button_new_with_label("Wyjscie");
-    g_signal_connect (button, "clicked", G_CALLBACK (back), NULL);
+    button = gtk_button_new_with_label("Powrot");
+    g_signal_connect (button, "clicked", G_CALLBACK (init_menu), NULL);
 
     gtk_widget_set_size_request(button, 160, 32);
 
@@ -83,10 +95,76 @@ void init_play()
             G_CALLBACK(gtk_main_quit), NULL);
 }
 
+//function saves settings and opens previous panel
+void settings_back()
+{
+    double dif_value = gtk_range_get_value(GTK_RANGE(slider1));
+    double len_value = gtk_range_get_value(GTK_RANGE(slider2));
+
+    if ((dif_value != difficulty) || (len_value != length))
+    {
+        difficulty = dif_value;
+        length = len_value;
+
+        save_settings();
+    }
+
+    init_menu();
+}
+
+//function saves settings to .json file
+void save_settings()
+{
+    json_object *dif;
+    json_object *len;
+    json_object *settings;
+    
+    settings = json_object_new_object();
+
+    dif = json_object_new_double(difficulty);
+    json_object_object_add(settings, "difficulty", dif);
+
+    len = json_object_new_double(length);
+    json_object_object_add(settings, "length", len);
+
+    json_object_to_file("settings.json", settings);
+}
+
+//function updates difficulty and length with values from settings file
+//if file doesn't exist default values are inserted
+void read_settings()
+{
+    json_object *dif;
+    json_object *len;
+    json_object *settings;
+
+    settings = json_object_from_file("settings.json");
+
+    if (settings)
+    {
+        dif = json_object_object_get(settings, "difficulty");
+        len = json_object_object_get(settings, "length");
+        difficulty = json_object_get_double(dif);
+        length = json_object_get_double(len);
+    }
+    else
+    {
+        //sets default values if file doesn't exist
+        difficulty = 1;
+        length = 1;
+    }
+    
+}
+
+//function initializing settings window
 void init_settings()
 {
-    GtkWidget *slider;
+    read_settings();
+
+    gtk_widget_destroy(fixed);
+
     GtkWidget *label;
+    GtkWidget *button;
 
     //initialize fixed
     fixed = gtk_fixed_new();
@@ -97,51 +175,48 @@ void init_settings()
     gtk_fixed_put(GTK_FIXED(fixed), label, 320, 130);
 
     //initialize first slider
-    slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+    slider1 = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+
+    gtk_scale_set_digits(GTK_SCALE(slider1), 0);
+
+    gtk_range_set_value(GTK_RANGE(slider1), difficulty);
 
     //set size
-    gtk_widget_set_size_request(slider, 160, 32);
+    gtk_widget_set_size_request(slider1, 160, 32);
 
-    gtk_fixed_put(GTK_FIXED(fixed), slider, 310, 150);
+    gtk_fixed_put(GTK_FIXED(fixed), slider1, 310, 150);
 
     label = gtk_label_new("Dlugosc gry");
     gtk_fixed_put(GTK_FIXED(fixed), label, 320, 230);
 
     //initialize second slider
-    slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+    slider2 = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 1, 3, 1);
+
+    gtk_scale_set_digits(GTK_SCALE(slider2), 0);
+
+    gtk_range_set_value(GTK_RANGE(slider2), length);
 
     //set size
-    gtk_widget_set_size_request(slider, 160, 32);
+    gtk_widget_set_size_request(slider2, 160, 32);
 
-    gtk_fixed_put(GTK_FIXED(fixed), slider, 310, 250);
+    gtk_fixed_put(GTK_FIXED(fixed), slider2, 310, 250);
+
+    //create "back button"
+    button = gtk_button_new_with_label("Powrot");
+    g_signal_connect (button, "clicked", G_CALLBACK (settings_back), NULL);
+
+    gtk_widget_set_size_request(button, 160, 32);
+
+    //attach fourth button 
+    gtk_fixed_put(GTK_FIXED(fixed), button, 310, 300);
 
     gtk_widget_show_all(window);
-}
-
-//menu button 1 (Play)
-void menu_b1(GtkWidget *button)
-{
-    gtk_widget_destroy(fixed);
-    init_play();
 }
 
 //menu button 2 (Leaderboard)
 void menu_b2(GtkWidget *button, GtkWidget *fixed)
 {
     // code //
-}
-
-//menu button 3 (Settings)
-void menu_b3(GtkWidget *button, GtkApplication *app)
-{
-    gtk_widget_destroy(fixed);
-    init_settings();
-}
-
-//menu button 4 ()
-void menu_b4(GtkWidget *button, GtkApplication *app)
-{
-    // code here //
 }
 
 void init_window()
@@ -158,6 +233,12 @@ void init_window()
 //function intializing menu window
 void init_menu()
 {
+    //destroy previous grid if it already exists
+    if (GTK_IS_WIDGET (fixed))
+    {
+        gtk_widget_destroy(fixed);
+    }
+
     GtkWidget *button;
 
     //initialize fixed
@@ -167,7 +248,7 @@ void init_menu()
 
     //create first button
     button = gtk_button_new_with_label("Graj");
-    g_signal_connect (button, "clicked", G_CALLBACK (menu_b1), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK (init_play), NULL);
 
     gtk_widget_set_size_request(button, 160, 32);
 
@@ -185,7 +266,7 @@ void init_menu()
 
     //create third button
     button = gtk_button_new_with_label("Ustawienia");
-    g_signal_connect (button, "clicked", G_CALLBACK (menu_b3), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK (init_settings), NULL);
 
     gtk_widget_set_size_request(button, 160, 32);
 
@@ -213,14 +294,6 @@ int main(int argc, char *argv[])
     srand(time(NULL));
 
     gtk_init(&argc, &argv);
-
-    // GtkApplication *app;
-    // int status;
-
-    // app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    // g_signal_connect (app, "activate", G_CALLBACK (init_menu), NULL);
-    // status = g_application_run (G_APPLICATION (app), argc, argv);
-    // g_object_unref (app);
 
     init_window();
 
